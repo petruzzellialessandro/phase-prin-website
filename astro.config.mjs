373803ff2ab4f3481489d9c177f479cae7ea66c9
petruzzellialessandro <petruzzellialessandro2@gmail.com>
2026-02-25 10:24:49 +0100

    Configure Astro for GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://petruzzellialessandro.github.io',
+  base: 'phase-prin-website',
   vite: {
     plugins: [tailwindcss()]
   }
